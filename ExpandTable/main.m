3435dcba869fe7c34b322ba2cb90d14f4db95072
dazuiba <come2u@gmail.com>
2013-04-10 18:25:07 +0800

    Initial Commit

---
+++ ExpandTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExpandTable
+//
+//  Created by Guangyu Zhang on 4/10/13.
+//  Copyright (c) 2013 Guangyu Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
